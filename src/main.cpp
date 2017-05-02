9a42ebc16742a8bd2a36feac29d6c484739d062b
Denis Krut <denis.krut@gmail.com>
2017-05-01 23:36:43 -0700

    Use steering angle to control throttle

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = (1. - std::abs(angle)/25) * 0.8 + 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
